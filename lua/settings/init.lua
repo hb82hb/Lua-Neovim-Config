1d3b5d79de4242070b7ca892bab1af12d18ffbf1
Ben De Bonis <ben.debonis@gmail.com>
2022-07-17 13:24:44 -0700

    <Space>m executes :MarkdownPreview

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -33,3 +33,14 @@ augroup AutoCompileMarkdown
 autocmd BufEnter,BufRead *.md :set spell
 augroup END 
 ]])
+
+-- Functional wrapper for mapping custom keybindings
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map("n", "<Space>m", ":MarkdownPreview<CR>")
